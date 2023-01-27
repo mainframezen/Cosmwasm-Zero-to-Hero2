70030e7d4af0869fef0a916f9b221b0ac0ad0c54
mainframezen <ribeirolog@gmail.com>
2023-01-27 21:58:55 +0000

    assert_eq on 6???

--- src/contract.rs
+++ src/contract.rs
@@ -45,4 +45,55 @@ pub fn query(_deps: Deps, _env: Env, _msg: QueryMsg) -> StdResult<Binary> {
 }
 
 #[cfg(test)]
-mod tests {}
+mod tests {
+    use cosmwasm_std::attr; // helper to construct an attribute e.g. ("action", "instantiate")
+    use cosmwasm_std::testing::{mock_dependencies, mock_env, mock_info}; // mock functions to mock an environment, message info, dependencies
+    use crate::contract::instantiate; // the contract instantiate function
+    use crate::msg::InstantiateMsg; // our instantate method
+
+    // Two fake addresses we will use to mock_info
+    pub const ADDR1: &str = "addr1";
+    pub const ADDR2: &str = "addr2";
+
+    #[test]
+    fn test_instantiate() {
+        // Mock the dependencies, must be mutable so we can pass it as a mutable, empty vector means our contract has no balance
+        let mut deps = mock_dependencies();
+        // Mock the contract environment, contains the block info, contract address, etc.
+        let env = mock_env();
+        // Mock the message info, ADDR1 will be the sender, the empty vec means we sent no funds.
+        let info = mock_info(ADDR1, &vec![]);
+
+        // Create a message where we (the sender) will be an admin
+        let msg = InstantiateMsg { admin: None };
+        // Call instantiate, unwrap to assert success
+        let res = instantiate(deps.as_mut(), env, info, msg).unwrap();
+
+        assert_eq!(
+            res.attributes,
+            vec![attr("action", "instantiate"), attr("admin", ADDR1)]
+        )
+    }
+
+    #[test]
+    fn test_instantiate_with_admin() {
+        // Mock the dependencies, must be mutable so we can pass it as a mutable, empty vector means our contract has no balance
+        let mut deps = mock_dependencies();
+        // Mock the contract environment, contains the block info, contract address, etc.
+        let env = mock_env();
+        // Mock the message info, ADDR1 will be the sender, the empty vec means we sent no funds.
+        let info = mock_info(ADDR2, &vec![]);
+
+        // Create a message where we (the sender) will be an admin
+        let msg = InstantiateMsg { admin: Some("Boss".to_string()) };
+        // Call instantiate, unwrap to assert success
+        let res = instantiate(deps.as_mut(), env, info, msg).unwrap();
+
+        assert_eq!(
+            res.attributes,
+            vec![attr("action", "instantiate_width_admin"), attr("admin", ADDR2)]
+        )
+    }
+
+    
+}
